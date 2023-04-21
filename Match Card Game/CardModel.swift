3eac2fa50170812478288a3414060029ba68f138
mykola <mykola.sosula@gmail.com>
2023-04-21 16:37:31 +0300

    fix for generating unique card pairs

--- Match Card Game/CardModel.swift
+++ Match Card Game/CardModel.swift
@@ -18,25 +18,6 @@ class CardModel {
         var generatedNumbers = [Int]()
         
         // Randomly generate 8 pairs of cards
-//        for _ in 1...8 {
-//
-//            // Generate a random number
-//            let randomNumber = Int.random(in: 1...13)
-//
-//            // Create two new card object
-//            let cardOne = Card()
-//            let cardTwo = Card()
-//
-//            // Set their names
-//            cardOne.imageName = "card\(randomNumber)"
-//            cardTwo.imageName = "card\(randomNumber)"
-//
-//            // Add them to the array
-//            generatedCards += [cardOne, cardTwo]
-//
-//            print(randomNumber)
-//        }
-        
         repeat {
 
             // Generate random number
@@ -58,12 +39,13 @@ class CardModel {
                 generatedCards += [cardOne, cardTwo]
                 
                 print(randomNumber)
+
             }
-        } while(generatedCards.count < 8)
+        } while(generatedCards.count < 16)
         
         // Radomize the cards withing the array
         generatedCards.shuffle()
-        
+        print("Count", generatedCards.count)
         // Return the array
         return generatedCards
     }
