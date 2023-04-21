fff6065407055e6b55d435f168a4ac38b5741a09
mykola <mykola.sosula@gmail.com>
2023-04-21 14:01:26 +0300

    Init new project - added assets - start creating models of card

---
+++ Match Card Game/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Match Card Game
+//
+//  Created by Mykola on 21.04.2023.
+//
+
+import Foundation
+
+class Card {
+    
+    var imageName = ""
+    var isMatch = false
+    var isFlipped = false
+}
---
+++ Match Card Game/CardModel.swift
@@ -0,0 +1,23 @@
+//
+//  CardModel.swift
+//  Match Card Game
+//
+//  Created by Mykola on 21.04.2023.
+//
+
+import Foundation
+
+class CardModel {
+    
+    func getCards() -> [Card] {
+        
+        // Declera an empty array
+        var generatedCards = [Card]()
+        
+        // Randomly generate 8 pairs of cards
+        
+        // Radomize the cards withing the array
+        
+        // Return the array
+    }
+}
