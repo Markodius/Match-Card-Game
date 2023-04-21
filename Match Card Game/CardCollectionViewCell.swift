7e9a9b5c9035f7ea0a505ecc779c4823f1b9c99a
mykola <mykola.sosula@gmail.com>
2023-04-21 15:57:49 +0300

    added logic for generate unique cards without duplicates add outlets for from and back card images

---
+++ Match Card Game/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  Match Card Game
+//
+//  Created by Mykola on 21.04.2023.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+    
+}
--- Match Card Game/CardModel.swift
+++ Match Card Game/CardModel.swift
@@ -14,23 +14,53 @@ class CardModel {
         // Declera an empty array
         var generatedCards = [Card]()
         
+        // Array helper to track allready exists card
+        var generatedNumbers = [Int]()
+        
         // Randomly generate 8 pairs of cards
-        for _ in 1...8 {
-            
-            // Generate a random number
+//        for _ in 1...8 {
+//
+//            // Generate a random number
+//            let randomNumber = Int.random(in: 1...13)
+//
+//            // Create two new card object
+//            let cardOne = Card()
+//            let cardTwo = Card()
+//
+//            // Set their names
+//            cardOne.imageName = "card\(randomNumber)"
+//            cardTwo.imageName = "card\(randomNumber)"
+//
+//            // Add them to the array
+//            generatedCards += [cardOne, cardTwo]
+//
+//            print(randomNumber)
+//        }
+        
+        repeat {
+
+            // Generate random number
             let randomNumber = Int.random(in: 1...13)
             
-            // Create two new card object
-            let cardOne = Card()
-            let cardTwo = Card()
-            
-            // Set their names
-            cardOne.imageName = "card\(randomNumber)"
-            cardTwo.imageName = "card\(randomNumber)"
-            
-            // Add them to the array
-            generatedCards += [cardOne, cardTwo]
-        }
+            // Save number to array helper to shure we generate unique cards
+            if generatedNumbers.contains(randomNumber) != true {
+                generatedNumbers.append(randomNumber)
+                
+                // Create two new card objects
+                let cardOne = Card()
+                let cardTwo = Card()
+
+                // Set their images
+                cardOne.imageName = "card\(randomNumber)"
+                cardTwo.imageName = "card\(randomNumber)"
+                
+                // Add them to the cards array
+                generatedCards += [cardOne, cardTwo]
+                
+                print(randomNumber)
+            }
+        } while(generatedCards.count < 8)
+        
         // Radomize the cards withing the array
         generatedCards.shuffle()
         
