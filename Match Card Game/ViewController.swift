2b6babf25491b7f1d35b81b22c94d069e21eb8d7
mykola <mykola.sosula@gmail.com>
2023-04-27 14:32:41 +0300

    Add timer

--- Match Card Game/ViewController.swift
+++ Match Card Game/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     let model = CardModel()
     var cardsArray = [Card]()
+    
+    var timer: Timer?
+    var milliseconds: Int = 30 * 1000
     
     var firstFlippedCardIndex: IndexPath?
     var secondFlippedCard: IndexPath?
@@ -26,6 +31,30 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // Set the view controller as a data source and delegate of the collection view
         collectionView.dataSource = self
         collectionView.delegate = self
+        
+        // initialize the timer
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode: .common)
+    }
+    
+    // MARK: - Timer Methods
+    
+    @objc func timerFired() {
+        // Decrement counter
+        milliseconds -= 1
+        
+        // Update label
+        let seconds: Double = Double(milliseconds) / 1000.0
+        timerLabel.text = String(format: "Time Renaining: %.2f", seconds)
+        
+        // Stop the timer if it reach zero
+        if milliseconds == 0 {
+            timerLabel.textColor = UIColor.red
+            timer?.invalidate()
+            
+            // Check if the user has cleared all pairs
+            checkForGameEnd()
+        }
     }
     
     // MARK: - Collection View Delegate Methods
@@ -107,6 +136,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             
             cardOneCell?.remove()
             cardTwoCell?.remove()
+            
+            // Was that the last pair?
+            checkForGameEnd()
         }
         else {
             
@@ -121,5 +153,39 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         // Reset the firstFlippedCardIndex Property
         firstFlippedCardIndex = nil
+    }
+    
+    func checkForGameEnd() {
+        // Check if there's any card tht is on match
+        // Assume the user has won, loop throught all the cards to see if all of them are matched
+        var hasWon = true
+        
+        for card in cardsArray {
+            if card.isMatched == false {
+                hasWon = false
+                break
+            }
+        }
+        
+        if hasWon {
+            // user has won, show an alert
+            showAlert(title: "Congratulations!", message: "You've won the game!")
+        } else {
+            // User hasn't won yet. Check of there's any tine left
+            showAlert(title: "Time;s ", message: <#T##String#>)
+        }
+        
+    }
+    
+    func showAlert(title: String, message: String) {
+        // Create the alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // Add a button for the user to dismis it
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        // Show the alert
+        present(alert, animated: true, completion: nil)
     }
 }
